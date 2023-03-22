501f2d2a9b5edb200bf15735e364ca8d82f10bdb
LHyunn <dns05018@kakao.com>
2023-03-22 15:34:11 +0900

    modified:   modules/KIS/config_example.json 	modified:   modules/KIS/public_api.py 	modified:   "pages/\360\237\223\210Chart.py"

--- modules/KIS/public_api.py
+++ modules/KIS/public_api.py
@@ -4,43 +4,50 @@ import datetime
 import os 
 import os
 import pandas as pd
+import shutil
 
 def init_KEY(APP_KEY, APP_SECRET):
     if not os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
-        with open(os.path.join(os.path.dirname(__file__), "config_example.json"), "w") as f:
-            json.dump(json.load(open(os.path.join(os.path.dirname(__file__), "config.json"), "r")), f, ensure_ascii=False, indent=4)
-        raise Exception("config.json 파일이 생성되었습니다.")
+        shutil.copy(os.path.join(os.path.dirname(__file__), "config_example.json"), os.path.join(os.path.dirname(__file__), "config.json"))
     CONFIG = json.load(open(os.path.join(os.path.dirname(__file__), "config.json"), "r"))
     CONFIG["UserConfig"]["APP_KEY"] = APP_KEY
     CONFIG["UserConfig"]["APP_SECRET"] = APP_SECRET
     with open(os.path.join(os.path.dirname(__file__), "config.json"), "w") as f:
         json.dump(CONFIG, f, ensure_ascii=False, indent=4)
-    return True
+    return "config.json 파일이 생성되었습니다."
+
+def delete_KEY():
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
+        os.remove(os.path.join(os.path.dirname(__file__), "config.json"))
+    return "config.json 파일이 삭제되었습니다."
 
 class Public_API:
     def __init__(self):
-        self.CONFIG = json.load(open(os.path.join(os.path.dirname(__file__), "config.json"), "r"))
-        self._ACCESS_TOKEN = self.CONFIG["UserConfig"]["ACCESS_TOKEN"]
-        self.ACCESS_TOKEN_EXPIRE = self.CONFIG["UserConfig"]["ACCESS_TOKEN_EXPIRE"]
-        self._APP_KEY = self.CONFIG["UserConfig"]["APP_KEY"]
-        self._APP_SECRET = self.CONFIG["UserConfig"]["APP_SECRET"]
-        self.auth_result = self.접근토큰발급()
+        if os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
+            self.CONFIG = json.load(open(os.path.join(os.path.dirname(__file__), "config.json"), "r"))
+            self._ACCESS_TOKEN = self.CONFIG["UserConfig"]["ACCESS_TOKEN"]
+            self._ACCESS_TOKEN_EXPIRE = self.CONFIG["UserConfig"]["ACCESS_TOKEN_EXPIRE"]
+            self._APP_KEY = self.CONFIG["UserConfig"]["APP_KEY"]
+            self._APP_SECRET = self.CONFIG["UserConfig"]["APP_SECRET"]
+            self.auth_result = self.접근토큰발급()
+        else:
+            raise Exception("config.json 파일이 존재하지 않습니다.")
         
     def 접근토큰발급(self):
-        if self.APP_KEY == "" or self.APP_SECRET == "":
-            raise Exception("APP_KEY 또는 APP_SECRET이 설정되지 않았습니다.")
-        if self.ACCESS_TOKEN == "" or datetime.datetime.strptime(self.ACCESS_TOKEN_EXPIRE,"%Y%m%d%H%M%S") < datetime.datetime.now() :
+        if self._APP_KEY == "" or self._APP_SECRET == "":
+            raise Exception("_APP_KEY 또는 APP_SECRET이 설정되지 않았습니다.")
+        if self._ACCESS_TOKEN == "" or datetime.datetime.strptime(self._ACCESS_TOKEN_EXPIRE,"%Y%m%d%H%M%S") < datetime.datetime.now() :
             headers = {"content-type":"application/json"}
             body = {
                 "grant_type" : "client_credentials",
-                "appkey" : self.APP_KEY,
-                "appsecret" : self.APP_SECRET
+                "appkey" : self._APP_KEY,
+                "appsecret" : self._APP_SECRET
                 }
             res = requests.post(self.CONFIG["URL"]["접근토큰발급(P)"], headers=headers, data=json.dumps(body))
             try:
                 ACCESS_TOKEN = res.json()["access_token"]
             except:
-                raise Exception("APP_KEY 또는 APP_SECRET이 잘못되었습니다.")
+                raise Exception("_APP_KEY 또는 APP_SECRET이 잘못되었습니다.")
             ACCESS_TOKEN_EXPIRE = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(days=1), "%Y%m%d%H%M%S")
             self.CONFIG["UserConfig"]["ACCESS_TOKEN"] = ACCESS_TOKEN
             self.CONFIG["UserConfig"]["ACCESS_TOKEN_EXPIRE"] = ACCESS_TOKEN_EXPIRE
@@ -53,9 +60,9 @@ class Public_API:
     def 국내주식기간별시세(self, stock_code, start_date, end_date, period_div_code="D"):
         headers = {
             "content-type" : "application/json",
-            "authorization" : "Bearer " + self.ACCESS_TOKEN,
-            "appkey" : self.APP_KEY,
-            "appsecret" : self.APP_SECRET,
+            "authorization" : "Bearer " + self._ACCESS_TOKEN,
+            "appkey" : self._APP_KEY,
+            "appsecret" : self._APP_SECRET,
             "tr_id" : "FHKST03010100",
             "custtype" : "P"
         }
@@ -73,9 +80,9 @@ class Public_API:
     def 주식현재가시세(self, stock_code):
         headers = {
             "content-type" : "application/json; charset=utf-8",
-            "authorization" : "Bearer " + self.ACCESS_TOKEN,
-            "appkey" : self.APP_KEY,
-            "appsecret" : self.APP_SECRET,
+            "authorization" : "Bearer " + self._ACCESS_TOKEN,
+            "appkey" : self._APP_KEY,
+            "appsecret" : self._APP_SECRET,
             "tr_id" : "FHKST01010100",
             "custtype" : "P"
         }
--- pages/📈Chart.py
+++ pages/📈Chart.py
@@ -9,6 +9,7 @@ import FinanceDataReader as fdr
 st.markdown("# 📈Chart")
 stock_code_list = stock.get_stock_list()
 stock_code = st.selectbox("종목코드", stock_code_list)
+public_api.init_KEY(APP_KEY="PS3RGjPgZhiW28Tq89ba75qkGPCiUGp0b6kf", APP_SECRET="oDe/hWTSx8CL7OP/tLhI8UMiPwNyumhXHnfd3iatGDNqB/N1vfSdSldqFAxD3CHd9rhZIjkafOSPIzVi48vmdMrweaGOMVvkao8kam12pCGcYd5pkVGUiZhW8A969j6FiJNVj1zkLYsILfB6gbot+jf9ZBdshzbR5F+f3XfEzB5EGyl9+wU=")
 API = public_api.Public_API()
 if st.button("조회"):
     stock_ohlcv = API.주식현재가시세(stock_code)
