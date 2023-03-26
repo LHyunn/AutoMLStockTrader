a2035a2de1eacb3edecfe9b6b1776cd7f4d05edf
LHyunn <dns05018@kakao.com>
2023-03-27 03:41:48 +0900

    modified:   .gitignore 	new file:   "pages/\360\237\224\221Set Config.json.py"

---
+++ pages/🔑Set Config.json.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import plotly.express as px
+from modules.KIS import public_api, stock, auth
+import FinanceDataReader as fdr
+
+st.markdown("# 🔑Set Config.json")
+APP_KEY = st.text_input("APP_KEY")
+SECRET_KEY = st.text_input("SECRET_KEY")
+
+if st.button("Create New Config.json"):
+    st.write(auth.init_KEY(APP_KEY, SECRET_KEY))
